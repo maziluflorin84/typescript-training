45022c687844f68ca1dfa1239b5b33c2cd9d70d0
Florin Mazilu <maziluflorin84@outlook.com>
2022-09-03 02:00:26 +0300

    Removed .js files

--- 04-classes/05-modules/Driver.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Customer_1 = require("./Customer");
-// let's create an instance
-let myCustomer = new Customer_1.Customer("Florin", "Mazilu");
-console.log(`First Name: ${myCustomer.firstName}`);
-console.log(`Last Name: ${myCustomer.lastName}`);
